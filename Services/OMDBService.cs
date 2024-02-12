369960dfff6a4ddb810c36d5a19122dda8b8f73b
Matthew Knorr <matt@knorrs.net>
2024-02-12 15:34:43 -0700

    Updated Api Display's Multiple Titles

--- Services/OMDBService.cs
+++ Services/OMDBService.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -14,23 +15,38 @@ public class OMDBService
         _apiKey = "9275f68f"; // OMDB API key
     }
 
-    public async Task<MovieInfo> GetMovieInfoAsync(string title)
+    public async Task<List<MovieInfo>> SearchMoviesAsync(string query)
     {
         try
         {
-            var response = await _httpClient.GetAsync($"http://www.omdbapi.com/?t={Uri.EscapeDataString(title)}&apikey={_apiKey}");
+            var response = await _httpClient.GetAsync($"http://www.omdbapi.com/?s={Uri.EscapeDataString(query)}&apikey={_apiKey}");
 
             if (response.IsSuccessStatusCode)
             {
                 var content = await response.Content.ReadAsStringAsync();
-                var movieInfo = JsonSerializer.Deserialize<MovieInfo>(content);
-                return movieInfo;
+                var movieSearchResult = JsonSerializer.Deserialize<MovieSearchResult>(content);
+
+                if (movieSearchResult != null && movieSearchResult.Search != null)
+                {
+                    var movieInfos = new List<MovieInfo>();
+
+                    foreach (var movie in movieSearchResult.Search)
+                    {
+                        var detailedResponse = await _httpClient.GetAsync($"http://www.omdbapi.com/?i={movie.imdbID}&apikey={_apiKey}");
+                        if (detailedResponse.IsSuccessStatusCode)
+                        {
+                            var detailedContent = await detailedResponse.Content.ReadAsStringAsync();
+                            var movieInfo = JsonSerializer.Deserialize<MovieInfo>(detailedContent);
+                            movieInfos.Add(movieInfo);
+                        }
+                    }
+
+                    return movieInfos;
+                }
             }
-            else
-            {
-                // Handle unsuccessful response
-                return null;
-            }
+
+            // Handle unsuccessful response
+            return null;
         }
         catch (Exception ex)
         {
@@ -46,5 +62,18 @@ public class MovieInfo
     public string Title { get; set; }
     public string Year { get; set; }
     public string Plot { get; set; }
+    // Add more properties as needed
+}
+
+public class MovieSearchResult
+{
+    public List<MovieSearchItem> Search { get; set; }
+}
+
+public class MovieSearchItem
+{
+    public string Title { get; set; }
+    public string Year { get; set; }
+    public string imdbID { get; set; }
     // Add more properties as needed
 }
